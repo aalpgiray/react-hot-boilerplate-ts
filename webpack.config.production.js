adc3986f8a35f94a39aa91f3b487e84ab2189b6d
Aykut Alpgiray Ateş <aykutalpgiray@outlook.com>
2016-08-07 19:10:20 +0300

    .

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -44,6 +44,15 @@ module.exports = {
     }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
-    })
+    }),
+     new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      output: {
+        comments: false
+      },
+      sourceMap: false
+    }),
   ]
 }
